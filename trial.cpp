40972438f74b988e7e1501213b9938f9109c225c
Iftkharvai <110301550+Iftkharvai@users.noreply.github.com>
2022-07-30 23:19:04 +0600

    Add files via upload

---
+++ trial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+char x;
+int a,b,c;
+
+a = 5;
+b = 6;
+c = 9;
+
+cout<< "The Result Is  : "<<endl;
+
+
+cout<<a*b*c<<endl;
+
+return 0;
+
+
+}
+
